c9cefef6d61277b00f4b5e2726a913d9825fbfd3
YAOQISEN <605217467@qq.com>
2021-02-24 15:37:05 +0800

    新module

---
+++ com.atguigu.springcloud/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * @author yqs 2021/2/23
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
