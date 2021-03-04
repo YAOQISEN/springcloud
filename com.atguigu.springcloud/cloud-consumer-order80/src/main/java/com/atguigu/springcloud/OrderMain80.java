ce847ffd535be9c9d390126d03cca8109cc839bd
YAOQISEN <605217467@qq.com>
2021-03-04 09:06:02 +0800

    add：消费者订单模块

---
+++ com.atguigu.springcloud/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yqs 2021/2/25
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+
+}
--- com.atguigu.springcloud/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ com.atguigu.springcloud/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -34,7 +34,7 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         log.info("*****查询结果："+payment);
         if (payment!=null){  //说明有数据，能查询成功
-            return new CommonResult(200,"查询成功",payment);
+            return new CommonResult(200,"查询成功 ",payment);
         }else {
             return new CommonResult(444,"没有对应记录，查询ID："+id,null);
         }
